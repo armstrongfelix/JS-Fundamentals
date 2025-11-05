acc2a0504fd9647cfa9ff655ef5b0f1da87bb6ee
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2025-11-05 07:56:48 +0100

    corrected

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,7 @@
-function printNames() {
-    const args = process.argv.slice(2);
-    const first = args[0];
-    const second = args[1];
-    console.log(`${first} is ${second}`);
+const args = process.argv.slice(2);
+const first = args[0];
+const second = args[1];
+
+// Print in the format: "<arg1> is <arg2>"
+// If an argument is missing, print undefined for that position (per typical JS behavior)
+console.log(`${first} is ${second}`);
