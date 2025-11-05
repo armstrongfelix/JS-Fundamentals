5e1268b3b06ddc3559687d318e8df137d2b6fbe2
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2025-11-05 07:47:18 +0100

    corrected 4-concat.js file

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,7 @@
-let name1 = "Felix";
-let nmae2 = "Armstrong";
+let name1 = "HBTN";
+let nmae2 = "Python fun";
 
 function printNanes(name1, name2) {
   console.log(name1 + " is " + name2);
 }
+printNanes(name1,nmae2)
